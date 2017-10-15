9b49c637ee91f1d2963793005092dc88315cd2f7
Loyd Charles <loydcharles3@gmail.com>
2017-10-15 19:07:38 -0400

    fix URI

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/mongoosedb");
+mongoose.connect("MONGODB_URI");
 var db = mongoose.connection;
 
 // Show any mongoose errors
